e37ff060be487c6fa48a78b1840d7bd83dd5e307
Admin <Admin@DESKTOP-9ODG3UU>
2023-09-08 15:37:44 +0530

    First Commit

---
+++ src/com/lbrce/devops/Sample.java
@@ -0,0 +1,14 @@
+package com.lbrce.devops;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is default code");
+	}
+	public static void fun() {
+		// TODO Auto-generated method stub
+		System.out.println("This is fun code");
+	}
+
+}
